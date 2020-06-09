a50eb7a8b4c68ac825aa461a971ecd26e3fe0a64
adiogoti <andrei.diogoti@endava.com>
2020-06-09 21:24:00 +0300

    CreateReport.java is added in util TestEventHandlerPlugin plugin is added.

---
+++ src/main/java/com/demoqa/shop/plugins/TestEventHandlerPlugin.java
@@ -0,0 +1,27 @@
+package com.demoqa.shop.plugins;
+
+import com.demoqa.shop.util.CreateReport;
+import io.cucumber.plugin.ConcurrentEventListener;
+import io.cucumber.plugin.event.EventHandler;
+import io.cucumber.plugin.event.EventPublisher;
+import io.cucumber.plugin.event.TestRunFinished;
+
+import java.io.File;
+import java.util.Collections;
+
+
+public class TestEventHandlerPlugin implements ConcurrentEventListener {
+
+    @Override
+    public void setEventPublisher(EventPublisher eventPublisher) {
+        eventPublisher.registerHandlerFor(TestRunFinished.class, teardown);
+    }
+
+    private EventHandler<TestRunFinished> teardown = event -> {
+        //run code after all tests
+        new CreateReport().generateReportForJsonFiles(new File("target/"),
+                Collections.singletonList("target/cucumber.json"));
+    };
+
+}
+
--- src/test/java/com/demoqa/shop/runners/RunCukeTests.java
+++ src/test/java/com/demoqa/shop/runners/RunCukeTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith( Cucumber.class )
 @CucumberOptions(
-        plugin = {"pretty", "html:target/report", "json:target/cucumber.json"},
+        plugin = {"pretty", "html:target/report", "json:target/cucumber.json", "com.demoqa.bdd.runners.TestEventHandlerPlugin"},
         features = {"src/test/resources/com/demoqa/shop/features"},
         glue = {"src/test/java/com/demoqa/shop/hooks",
                 "src/test/java/com/demoqa/shop/steps"},
