b0b77a297f47c7bd28055577a49dddde25221dca
Natalia Lasco <natalia.lasco@endava.com>
2020-06-17 12:22:39 +0300

    LogBack update, Chechout page updates

--- src/test/java/com/demoqa/shop/steps/CheckoutStepdefs.java
+++ src/test/java/com/demoqa/shop/steps/CheckoutStepdefs.java
@@ -7,8 +7,12 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,10 +25,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CheckoutStepdefs {
     Logger log = LoggerFactory.getLogger(CheckoutStepdefs.class);
+    WebDriverWait wait = new WebDriverWait(Browser.getBrowser(), 20);
 
     @Given( "user clicks Terms checkbox" )
     public void clickCheckbox() {
         CheckoutPage page = (CheckoutPage) ScenarioContext.getInstance().getContext(Context.CURRENT_PAGE);
+        wait.until(ExpectedConditions.elementToBeClickable(page.getTermsCheckbox()));
         page.clickTerms();
         log.info("user clicked Terms checkbox");
     }
@@ -34,6 +40,7 @@ public class CheckoutStepdefs {
         CheckoutPage page = (CheckoutPage) ScenarioContext.getInstance().getContext(Context.CURRENT_PAGE);
         WebElement loginSection = page.getLoginSection();
         ScreenshotUtil.highLight(loginSection);
+        ScreenshotUtil.scrollToElement(loginSection);
         ScreenshotUtil.takeScreenshot("Login section");
         ATFAssert.assertTrue("\"Login\" sections was not displayed on checkout page", page.loginSectionIsDisplayed(), "\"Login\" section isDispalayed()");
     }
@@ -81,6 +88,7 @@ public class CheckoutStepdefs {
         CheckoutPage page = (CheckoutPage) ScenarioContext.getInstance().getContext(Context.CURRENT_PAGE);
         WebElement alert = page.getAlerts();
         ScreenshotUtil.highLight(alert);
+        ScreenshotUtil.scrollToElement(alert);
         ScreenshotUtil.takeScreenshot("Alert");
         ATFAssert.assertTrue("Wrong message is displayed", page.getAlertMessages().containsValue(string), "corresponding alert message is displayed");
     }
@@ -189,6 +197,7 @@ public class CheckoutStepdefs {
                     throw new IllegalStateException("Unexpected key for billing details of the customer: " + entry.getKey().toLowerCase());
             }
             ScreenshotUtil.highLight(actualElement);
+            ScreenshotUtil.scrollToElement(actualElement);
             ScreenshotUtil.takeScreenshot("Auto-fill of " + entry.getKey());
             ATFAssert.assertEquals(entry.getValue(), actual, "Field " + entry.getKey() + "did not fill correctly", "Field " + entry.getKey() + " was filled with correct data: " + entry.getValue());
         }
@@ -219,6 +228,7 @@ public class CheckoutStepdefs {
     public void termsCheckboxIsChecked() {
         CheckoutPage page = (CheckoutPage) ScenarioContext.getInstance().getContext(Context.CURRENT_PAGE);
         ScreenshotUtil.highLight(page.getTermsCheckbox());
+        ScreenshotUtil.scrollToElement(page.getTermsCheckbox());
         ScreenshotUtil.takeScreenshot("Terms");
         ATFAssert.assertTrue("\'Terms\' checkbox is not selected", page.termsIsSelected(), "Terms checkbox is selected");
     }
